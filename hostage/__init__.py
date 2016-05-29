f865312c29153955451f17147b98a6520fb58cdd
dhleong <falcone88@gmail.com>
2016-05-29 00:36:56 -0400

    Begin setup.py to track deps
    For when we're ready to publish

--- hostage/__init__.py
+++ hostage/__init__.py
@@ -1,6 +1,7 @@
 
 __title__ = 'hostage'
 __author__ = 'Daniel Leong'
+__version__ = '0.1.0'
 
 from .core import *
 from .evaluators import *
---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import re
+
+from setuptools import setup
+
+packages = ['hostage']
+
+requires = ['requests>=2.10.0']
+
+test_requirements = ['pytest>=2.8.0', 'responses>=0.5.1']
+
+
+with open('hostage/__init__.py', 'r') as fp:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                        fp.read(), re.MULTILINE).group(1)
+
+if not version:
+    raise RuntimeError('Cannot find version information')
+
+setup(
+  name='hostage',
+  version=version,
+  description="You'll WANT to release it",
+  author='Daniel Leong',
+  author_email='me@dhleong.net',
+  url='https://github.com/dhleong/hostage', # use the URL to the github repo
+  packages=packages, 
+  package_dir={'hostage': 'hostage'},
+  install_requires=requires,
+  tests_require=test_requirements,
+  download_url='https://github.com/dhleong/hostage/tarball/' + version, 
+  keywords=['release', 'script'], 
+  classifiers=[],
+)
