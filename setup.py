c2bcb46d9861b2a125bde3286b71acc660809d2d
dhleong <falcone88@gmail.com>
2016-05-31 07:01:10 -0400

    Support ./python setup.py test for running tests

--- setup.py
+++ setup.py
@@ -1,8 +1,29 @@
 #!/usr/bin/env python
 
+import sys
 import re
 
 from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass into py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
 
 packages = ['hostage']
 
@@ -36,4 +57,5 @@ setup(
   download_url='https://github.com/dhleong/hostage/tarball/' + version, 
   keywords=['release', 'script'], 
   classifiers=[],
+  cmdclass={'test': PyTest}
 )
